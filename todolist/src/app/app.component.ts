9153d9a4bc317e481ff8474c38e82c930ad27357
Leandro Andrade <eusouleoandrade@gmail.com>
2021-09-13 08:20:56 -0300

    Add actions in the form.

--- todolist/src/app/app.component.ts
+++ todolist/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Todo } from 'src/models/todo.model';
 
 @Component({
@@ -10,8 +11,41 @@ export class AppComponent {
 
   public title : string = "Todo List - Making your life easier";
   public todos : Todo[] = [];
+  public form: FormGroup;
 
-  constructor() {
-    this.todos.push(new Todo(1, 'Tarefa exemplo', false));
+  constructor(private fb: FormBuilder) {
+    
+    this.form = this.fb.group({
+      title: ['', Validators.compose([Validators.minLength(3), Validators.maxLength(60), Validators.required])]
+    });
+  }
+
+  add(){
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+
+    this.todos.push(new Todo(id, title, false));
+
+    this.clearForm();
+  }
+
+  clearForm(){
+    this.form.reset();
+  }
+
+  remove(todo : Todo){
+    const index = this.todos.indexOf(todo);
+
+    if(index !== -1){
+      this.todos.splice(index, 1);
+    }
+  }
+
+  markAsDone(todo : Todo){
+    todo.done = true;
+  }
+
+  markAsUnDone(todo : Todo){
+    todo.done = false;
   }
 }

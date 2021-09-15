72f8071080128cb7d5e1ef7da511582cb3ca6a9d
Leandro Andrade <eusouleoandrade@gmail.com>
2021-09-15 01:06:00 -0300

    Adiciona persistência no localStorage.

--- todolist/src/app/app.component.ts
+++ todolist/src/app/app.component.ts
@@ -9,45 +9,59 @@ import { Todo } from 'src/models/todo.model';
 })
 export class AppComponent {
 
-  public title : string = "Todo List - Making your life easier";
-  public todos : Todo[] = [];
+  public title: string = "Todo List - Making your life easier";
+  public todos: Todo[] = [];
   public form: FormGroup;
-  public formVisible : boolean = false;
+  public formVisible: boolean = false;
 
   constructor(private fb: FormBuilder) {
-    
+
     this.form = this.fb.group({
       title: ['', Validators.compose([Validators.minLength(3), Validators.maxLength(60), Validators.required])]
     });
+
+    const data = localStorage.getItem('todos');
+    if (data !== null) {
+      this.todos = JSON.parse(data);
+    }
   }
 
-  add(){
+  add() {
     const title = this.form.controls['title'].value;
     const id = this.todos.length + 1;
 
     this.todos.push(new Todo(id, title, false));
+    this.save();
 
     this.clearForm();
   }
 
-  clearForm(){
+  clearForm() {
     this.form.reset();
     this.formVisible = false;
   }
 
-  remove(todo : Todo){
+  remove(todo: Todo) {
     const index = this.todos.indexOf(todo);
 
-    if(index !== -1){
+    if (index !== -1) {
       this.todos.splice(index, 1);
+      this.save();
     }
   }
 
-  markAsDone(todo : Todo){
+  markAsDone(todo: Todo) {
     todo.done = true;
+    this.save();
   }
 
-  markAsUnDone(todo : Todo){
+  markAsUnDone(todo: Todo) {
     todo.done = false;
+    this.save();
+  }
+
+  save() {
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
   }
 }
